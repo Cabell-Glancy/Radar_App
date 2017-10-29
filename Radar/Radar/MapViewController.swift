889161315d1e8f5dc9108633f42b6bf7fd6d886a
student <student@d-172-26-137-75.dhcp.virginia.edu>
2017-10-29 14:48:33 -0400

    maptest, zooms into user location

---
+++ Radar/Radar/MapViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Radar
+//
+//  Created by student on 10/29/17.
+//  Copyright © 2017 cs4720. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        print(userLocation.coordinate)
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+        
+    }
+
+
+}
+
