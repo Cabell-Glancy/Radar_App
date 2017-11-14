048140877c1dea509da645d4702e475b533a8587
student <student@d-172-26-138-153.dhcp.virginia.edu>
2017-11-13 21:37:33 -0500

    updated userlocation

--- Radar/Radar/MapViewController.swift
+++ Radar/Radar/MapViewController.swift
@@ -11,8 +11,9 @@ import MapKit
 
 private let kMessageAnnotationName = "kMessageAnnotationName"
 
-class MapViewController: UIViewController, MKMapViewDelegate {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
+    var locationManager:CLLocationManager!
     @IBOutlet weak var mapView: MKMapView!
     
     var selectedMessage: Message?
@@ -22,13 +23,20 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
         mapView.showsUserLocation = true
         mapView.delegate = self
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
         
+        // Or, if needed, we can position map in the center of the view
+        mapView.center = view.center
         let message = Message(content: "Hello", duration: 50, distance: 50, filter: Filter.cute, location: CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude, longitude: mapView.userLocation.coordinate.longitude))
         
         let messageAnnotation = MessageAnnotation(message: message)
         
+        print("USERLOCATION:" + String(describing: mapView.userLocation.coordinate))
+        
         print("++++++++++++++++++++++" + String(describing: messageAnnotation.coordinate))
-        mapView.addAnnotation(messageAnnotation)
+        //mapView.addAnnotation(messageAnnotation)
+        //mapView.showAnnotations([messageAnnotation], animated: true)
         
     }
 
@@ -37,24 +45,59 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        print(userLocation.coordinate)
-        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mapView.setRegion(region, animated: true)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        determineCurrentLocation()
+        let myAnnotation: MKPointAnnotation = MKPointAnnotation()
+        myAnnotation.coordinate = CLLocationCoordinate2D(latitude: mapView.userLocation.coordinate.latitude, longitude: mapView.userLocation.coordinate.longitude)
+        myAnnotation.title = "Current location"
+        mapView.addAnnotation(myAnnotation)
+        
     }
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation {
-            return nil
-        }
+//    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+//        print(userLocation.coordinate)
+//        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+//        mapView.setRegion(region, animated: true)
+//    }
+    
+    func determineCurrentLocation()
+    {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
         
-        else {
-            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
-            annotationView.image = UIImage(named: "mapPin")
-            return annotationView
+        if CLLocationManager.locationServicesEnabled() {
+            //locationManager.startUpdatingHeading()
+            locationManager.startUpdatingLocation()
         }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        //manager.stopUpdatingLocation()
+        
+        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+        
+    }
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        if annotation is MKUserLocation {
+//            return nil
+//        }
+//
+//        else {
+//            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+//            return annotationView
+//        }
+//    }
 
 }
 
